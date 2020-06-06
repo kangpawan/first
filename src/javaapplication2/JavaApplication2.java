300c000be2828bb9ca43ffbad896f40ee1aabb1b
Pawanpreet kaur kang <kangpawa@sheridancollege.ca>
2020-06-06 16:55:54 -0400

    second commit

--- src/javaapplication2/JavaApplication2.java
+++ src/javaapplication2/JavaApplication2.java
@@ -17,6 +17,7 @@ public class JavaApplication2 {
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.println("Hello");
+        System.out.println("Hello Hello!");
     }
     
 }
